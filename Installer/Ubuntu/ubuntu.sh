82bb8379ad1e3905acf484b2ed71620a01976817
Ivoszbg <43310844+ivosz@users.noreply.github.com>
2020-09-05 12:11:00 +0300

    Update ubuntu.sh
    making the script to use chroot instead of proot and making it only support x86

--- Installer/Ubuntu/ubuntu.sh
+++ Installer/Ubuntu/ubuntu.sh
@@ -1,5 +1,5 @@
-#!/data/data/com.termux/files/usr/bin/bash
-pkg install wget -y 
+#!/bin/bash
+apk add wget
 folder=ubuntu-fs
 if [ -d "$folder" ]; then
 	first=1
@@ -9,29 +9,13 @@ tarball="ubuntu-rootfs.tar.xz"
 if [ "$first" != 1 ];then
 	if [ ! -f $tarball ]; then
 		echo "Download Rootfs, this may take a while base on your internet speed."
-		case `dpkg --print-architecture` in
-		aarch64)
-			archurl="arm64" ;;
-		arm)
-			archurl="armhf" ;;
-		amd64)
-			archurl="amd64" ;;
-		x86_64)
-			archurl="amd64" ;;	
-		i*86)
-			archurl="i386" ;;
-		x86)
-			archurl="i386" ;;
-		*)
-			echo "unknown architecture"; exit 1 ;;
-		esac
-		wget "https://github.com/Techriz/AndronixOrigin/blob/master/Rootfs/Ubuntu/${archurl}/ubuntu-rootfs-${archurl}.tar.xz?raw=true" -O $tarball
+		wget "https://github.com/Techriz/AndronixOrigin/blob/master/Rootfs/Ubuntu/i386/ubuntu-rootfs-i386.tar.xz?raw=true" -O $tarball
 fi
 	cur=`pwd`
 	mkdir -p "$folder"
 	cd "$folder"
 	echo "Decompressing Rootfs, please be patient."
-	proot --link2symlink tar -xJf ${cur}/${tarball}||:
+	tar -xJf ${cur}/${tarball}
 	cd "$cur"
 fi
 mkdir -p ubuntu-binds
@@ -42,39 +26,17 @@ cat > $bin <<- EOM
 cd \$(dirname \$0)
 ## unset LD_PRELOAD in case termux-exec is installed
 unset LD_PRELOAD
-command="proot"
-command+=" --link2symlink"
-command+=" -0"
-command+=" -r $folder"
+export PATH=$bin:/usr/bin:/usr/sbin:/bin:$PATH
+export HOME=/root
+busybox mount -o bind /proc ${folder}/proc
+busybox chroot ${folder} /bin/bash
 if [ -n "\$(ls -A ubuntu-binds)" ]; then
     for f in ubuntu-binds/* ;do
       . \$f
     done
 fi
-command+=" -b /dev"
-command+=" -b /proc"
-command+=" -b ubuntu-fs/root:/dev/shm"
-## uncomment the following line to have access to the home directory of termux
-#command+=" -b /data/data/com.termux/files/home:/root"
-## uncomment the following line to mount /sdcard directly to / 
-#command+=" -b /sdcard"
-command+=" -w /root"
-command+=" /usr/bin/env -i"
-command+=" HOME=/root"
-command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
-command+=" TERM=\$TERM"
-command+=" LANG=C.UTF-8"
-command+=" /bin/bash --login"
-com="\$@"
-if [ -z "\$1" ];then
-    exec \$command
-else
-    \$command -c "\$com"
-fi
 EOM
 
-echo "fixing shebang of $bin"
-termux-fix-shebang $bin
 echo "making $bin executable"
 chmod +x $bin
 echo "removing image for some space"
